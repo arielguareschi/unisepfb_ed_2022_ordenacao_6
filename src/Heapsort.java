4248026b22c48c42868fd942d7bc9bd109103606
Leonardo <leonardohartmann03@gmail.com>
2022-11-27 22:12:31 -0300

    trabalhog2

---
+++ src/Heapsort.java
@@ -0,0 +1,74 @@
+
+public class Heapsort {
+    private static int tam;
+    private static int[] vetor;
+    
+  
+    public static void heapsort(int[] array) {
+        vetor = array;          
+        tam = vetor.length - 1;
+        
+       
+        constroiHeap();
+        
+        for (int i = tam; i > 0; i--) {
+            troca(0, tam);     
+            tam -= 1;           
+            maxHeapifica(0);
+        }
+    }
+    
+
+    private static void constroiHeap() {
+
+        int meio = (int) (tam/2);
+        
+        for (int i = meio - 1; i >= 0; i--) {
+            maxHeapifica(i);
+        }
+    }
+    
+    private static void troca(int i, int j) {
+        int aux;
+
+        aux = vetor[i];
+        vetor[i] = vetor[j];
+        vetor[j] = aux;
+    }
+    
+
+    private static void maxHeapifica(int pai) {
+        int maior = pai,            
+            esquerda = 2 * pai + 1,     // Pega a posição do filho da esquerda
+            direita = 2 * pai + 2;  // filho da direita.
+        
+        if (esquerda <= tam && vetor[esquerda] > vetor[maior])
+            maior = esquerda;
+        
+
+        if (direita <= tam && vetor[direita] > vetor[maior])
+            maior = direita;
+        
+
+        if (maior != pai) {
+            troca(pai, maior); 
+            maxHeapifica(maior);  
+        }
+    }
+    
+
+    public static void main(String[] args) {
+        int[] vetorDesordenado = {9, 1, 8, 3, 7, 4, 6, 5};
+        
+
+        heapsort(vetorDesordenado);
+        
+        String saida = "";
+
+        for(int valor : vetorDesordenado) {
+            saida += valor + " ";
+        }
+        
+        System.out.println(saida);
+    }
+}
